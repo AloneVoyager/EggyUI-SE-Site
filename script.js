151004b1cbac5a55baa84ea9c71b7c85ea3acf5f
ColdFieldObserver <cfobserver@hotmail.com>
2026-05-31 16:23:09 +0800

    Fix

--- script.js
+++ script.js
@@ -113,7 +113,7 @@ closeBtn.addEventListener('click', () => {
     
     setTimeout(() => {
         // 关闭标签页
-        window.close();
+        window.open('', '_self').close();
         
         // 备用方案：如果无法关闭，则隐藏容器
         setTimeout(() => {
